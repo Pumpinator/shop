0e58ee6bd96b3742223c4f204c540438c0ff7e39
Pumpinator <alejandrodcardona5@gmail.com>
2023-12-11 20:53:21 -0600

    Insert Roles Hibernate Testing

--- ShopWebParent/ShopBackEnd/src/test/java/com/shop/admin/user/RoleRepositoryTests.java
+++ ShopWebParent/ShopBackEnd/src/test/java/com/shop/admin/user/RoleRepositoryTests.java
@@ -9,16 +9,30 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import com.shop.common.entity.Role;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
 public class RoleRepositoryTests {
     @Autowired
     private RoleRepository roleRepository;
+
     @Test
     public void testCreateFirstRole() {
         Role roleAdmin = new Role("Admin", "Manage everything");
         Role savedRole = roleRepository.save(roleAdmin);
         assertThat(savedRole.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testCreateRestRoles() {
+        Role roleSalesperson = new Role("Salesperson", "Manage products price, customers, shipping, orders and sales reports");
+        Role roleEditor = new Role("Editor", "Manage categories, brands, products, articles and menus");
+        Role roleShipper = new Role("Shipper", "View products, orders and update orders status");
+        Role roleAssistant = new Role("Assistant", "Manage questions and reviews");
+        roleRepository.saveAll(List.of(roleSalesperson, roleEditor, roleShipper, roleAssistant));
     }
 }
