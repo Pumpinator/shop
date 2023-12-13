fb16f6cf9f97881bd2a262847c6351d3099a8057
Pumpinator <alejandrodcardona5@gmail.com>
2023-12-12 20:49:01 -0600

    Saving Encoded Passwords on Database

--- ShopWebParent/ShopBackEnd/src/main/java/com/shop/admin/user/UserService.java
+++ ShopWebParent/ShopBackEnd/src/main/java/com/shop/admin/user/UserService.java
@@ -3,6 +3,7 @@ package com.shop.admin.user;
 import com.shop.common.entity.Role;
 import com.shop.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +14,8 @@ public class UserService {
     private UserRepository userRepository;
     @Autowired
     private RoleRepository roleRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public List<User> getAll() {
         return (List<User>) userRepository.findAll();
@@ -23,6 +26,12 @@ public class UserService {
     }
 
     public void save(User user) {
+        encodePassword(user);
         userRepository.save(user);
+    }
+
+    private void encodePassword(User user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
     }
 }
