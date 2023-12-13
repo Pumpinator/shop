472b3af5cc20ec0de7958b11169c411b64201b6d
Pumpinator <alejandrodcardona5@gmail.com>
2023-12-12 20:35:21 -0600

    Spring Security Configuation Permit All Requests

---
+++ ShopWebParent/ShopBackEnd/src/main/java/com/shop/admin/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.shop.admin.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
+            authorizationManagerRequestMatcherRegistry.anyRequest().permitAll();
+        });
+        return httpSecurity.build();
+    }
+
+}
